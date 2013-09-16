839e6e1da8c0950eb24b39f5ddd5dbec59a56ada
Kiryl Baranoshnik <kiryl.baranoshnik@gmail.com>
2013-09-16 16:27:58 +0300

    Forget to clean up some comile stuff last time.

--- directives-lifecycle/script.js
+++ directives-lifecycle/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var compileTag = -1, linkTag = -1;
+var linkTag = -1;
 
 angular.module('tngDirectiveLifecycle', [])
 .directive('tngLifecycle', function() {
@@ -8,8 +8,8 @@ angular.module('tngDirectiveLifecycle', [])
 	return {
 		link: function(scope, iElement, iAttrs, controller) {
 
-			iElement.addClass('pretty');
-			console.log('[tng-lifecycle]: post-link');
+			// iElement.addClass('pretty');
+			// console.log('[tng-lifecycle]: post-link');
 			// console.log('[tng-lifecycle]: tElement === iElement? => ' + (tElement === iElement));
 
 			iElement.append('<span>link tag: ' + ++linkTag + '</span>');
