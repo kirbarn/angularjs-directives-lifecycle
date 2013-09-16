82adf03617ee842d9e545c79f0ad4f1e051cfe25
Kiryl Baranoshnik <kiryl.baranoshnik@gmail.com>
2013-09-16 15:18:20 +0300

    The structure could be even simpler. Also added some beauty to the presentation.

---
+++ directives-lifecycle/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var compileTag = -1, linkTag = -1;
+
+angular.module('tngDirectiveLifecycle', [])
+.directive('tngLifecycle', function() {
+
+	console.log('[tng-lifecycle]: constructor');
+
+	return {
+		controller: function($scope, $element, $attrs) {
+			console.log('[tng-lifecycle]: controller');
+			$element.addClass('controller');
+		},
+		compile: function(tElement, tAttrs) {
+			console.log('[tng-lifecycle]: compile');
+
+			tElement.addClass('pretty');
+			tElement.append('<span>compile tag: ' + ++compileTag + '</span><br>');
+
+			return {
+				pre: function (scope, iElement, iAttrs, controller) {
+					console.log('[tng-lifecycle]: pre-link');
+				},
+				post: function (scope, iElement, iAttrs, controller) {
+					console.log('[tng-lifecycle]: post-link');
+					console.log('[tng-lifecycle]: tElement === iElement? => ' + (tElement === iElement));
+
+					iElement.addClass('link' + iAttrs['index']);
+					iElement.append('<span>link tag: ' + ++linkTag + '</span>');
+				}
+			};
+		}
+	};
+})
+.directive('tngLifecycleChild', function() {
+	console.log('[tng-lifecycle-child]: constructor');
+
+	return {
+		controller: function($scope, $element, $attrs) {
+			console.log('[tng-lifecycle-child]: controller');
+		},
+		compile: function(tElement, tAttrs) {
+			console.log('[tng-lifecycle-child]: compile');
+
+			return {
+				pre: function (scope, iElement, iAttrs, controller) {
+					console.log('[tng-lifecycle-child]: pre-link');
+				},
+				post: function (scope, iElement, iAttrs, controller) {
+					console.log('[tng-lifecycle-child]: post-link');
+				}
+			};
+		}
+	};
+});
