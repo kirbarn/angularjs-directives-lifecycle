3ff5a9788a536a3bc0077ac71d9d089ea94c5a0e
Kiryl Baranoshnik <kiryl.baranoshnik@gmail.com>
2013-09-30 19:55:11 +0300

    More verbose text messages.

--- directives-lifecycle/script.js
+++ directives-lifecycle/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var linkTag = -1, compileTag = -1;
+var linkTag = 0, compileTag = 0;
 
 angular.module('tngDirectiveLifecycle', [])
 .directive('tngLifecycle', function() {
@@ -10,13 +10,13 @@ angular.module('tngDirectiveLifecycle', [])
 			console.log('[tng-lifecycle]: compile');
 
 			tElement.addClass('pretty');
-			tElement.append('compile: ' + ++compileTag + '<br>');
+			tElement.append('<code>compile()</code> call order: <b>' + ++compileTag + '</b><br>');
 
 			return function(scope, iElement) {
 				console.log('[tng-lifecycle]: link');
 
 				// iElement.prepend('<h3>' + scope.i + '</h3>');
-				iElement.append('link: ' + ++linkTag + '<br>');
+				iElement.append('<code>link()</code> call order: <b>' + ++linkTag + '</b><br>');
 				iElement.append('tElement === iElement? => ' + (tElement === iElement) + '<br>');
 			}
 		}
