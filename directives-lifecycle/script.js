69033a63ec2f2de5759c420f2251992ecbf11874
Kiryl Baranoshnik <kiryl.baranoshnik@gmail.com>
2013-09-30 19:57:02 +0300

    Correction.

--- directives-lifecycle/script.js
+++ directives-lifecycle/script.js
@@ -24,13 +24,13 @@ angular.module('tngDirectiveLifecycle', [])
 
 					console.log('[tng-lifecycle]: pre-link');
 
-					iElement.append('<code>pre()</code> (pre-link) call order <b>' + ++linkTag + '</b><br>');
+					iElement.append('<code>pre()</code> (pre-link) call order: <b>' + ++linkTag + '</b><br>');
 					iElement.append('*** tElement === iElement? => ' + (tElement === iElement) + '<br>');
 				},
 				post: function(scope, iElement) {
 					console.log('[tng-lifecycle]: post-link');
 
-					iElement.append('<code>post()</code> (post-link) call order <b>' + ++linkTag + '</b><br>');
+					iElement.append('<code>post()</code> (post-link) call order: <b>' + ++linkTag + '</b><br>');
 					iElement.append('*** tElement === iElement? => ' + (tElement === iElement) + '<br>');
 					iElement.append('*** preElement === postElement? => ' + (preElement === iElement));
 				}
